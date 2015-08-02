cb0385e861f47ffee3073efea6821de59b51ebe9
Andruxa <ogamespec@gmail.com>
2015-08-02 13:27:38 +0300

    Deroute checkin
    Initial commit

---
+++ tools/deroute/deroute.c
@@ -0,0 +1,8 @@
+#include <Windows.h>
+
+int CALLBACK WinMain(HINSTANCE Instance, HINSTANCE PrevInstance, LPSTR CmdLine, int CmdShow)
+{
+    MessageBox(NULL, "Hello", "Message", MB_OK);
+
+    return 0;
+}
